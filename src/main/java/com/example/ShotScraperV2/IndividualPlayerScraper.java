160585289046dbbe9bb83078b4d40e8a282f771f
Sam Nishita <saminator21@gmail.com>
2021-09-13 16:39:43 -0700

    Add Javadoc support

--- src/main/java/com/example/ShotScraperV2/IndividualPlayerScraper.java
+++ src/main/java/com/example/ShotScraperV2/IndividualPlayerScraper.java
@@ -1,5 +1,6 @@
 package com.example.ShotScraperV2;
 
+import org.apache.commons.logging.Log;
 import org.openqa.selenium.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,7 +12,10 @@ import java.util.*;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-public class IndividualPlayerScraper implements ScraperUtilsInterface{
+/**
+ * Scraper of all players
+ */
+public class IndividualPlayerScraper implements ScraperUtilsInterface {
     private Logger LOGGER = LoggerFactory.getLogger(IndividualPlayerScraper.class);
     private Connection connPlayers1 = null, connPlayers2 = null;
     private String schema1, schema2, location1, location2;
@@ -33,13 +37,21 @@ public class IndividualPlayerScraper implements ScraperUtilsInterface{
         LOGGER.info("Initialized PlayerInfoFinder for " + location1 + " and " + location2);
     }
 
+    /**
+     * Scrapes all active years for a given player
+     *
+     * @param reRun should ignore if player exists in database
+     */
     public void getAllActiveYearsUsingMain(boolean reRun) {
+        //Retired players who have no active years
         ArrayList<String> excludedIDs = new ArrayList(Arrays.asList("41", "986", "202070", "201195", "202221", "201998", "201987",
                 "202364", "202067", "202238", "1626122", "202358", "202392", "1629129", "202375"));
         //These players may make a comeback
         ArrayList<String> tempSkipIds = new ArrayList(Arrays.asList("1630258", "1629341", "1629624", "1630209", "1630492"));
         while (true) {
-            HashMap<String, String> eachPlayerHashMap = RunHandler.popConcurrentHashMap();
+            //Get the player to scrape data
+            //Exits while loop when popping returns null
+            HashMap<String, String> eachPlayerHashMap = RunHandler.popQueue();
             String playerTableName;
             if (eachPlayerHashMap == null) {
                 break;
@@ -54,6 +66,7 @@ public class IndividualPlayerScraper implements ScraperUtilsInterface{
                 }
                 playerTableName = lastName + "_" + firstName + "_" + eachID + "_individual_data";
                 int existingTableCount = 0;
+                //Check if table exists
                 try {
                     ResultSet rsTables = connPlayers1.getMetaData().getTables(this.schema1, null, playerTableName,
                             new String[]{"TABLE"});
@@ -63,12 +76,20 @@ public class IndividualPlayerScraper implements ScraperUtilsInterface{
                     int shouldReRun = reRun ? 1 : 0;
                     if (existingTableCount <= shouldReRun) {
                         try {
+                            //Fetch and parse the active seasons data
                             loopSearchIfError(eachID, firstName, lastName);
                             HashSet<String> allYears = new HashSet();
-                            this.regSeasons.stream().filter(eachSeason -> Integer.parseInt(eachSeason.substring(0, 4)) >= 1996).forEach(eachSeason -> allYears.add(eachSeason));
-                            this.postSeasons.stream().filter(eachSeason -> Integer.parseInt(eachSeason.substring(0, 4)) >= 1996).forEach(eachSeason -> allYears.add(eachSeason));
-                            this.preSeasons.stream().filter(eachSeason -> Integer.parseInt(eachSeason.substring(0, 4)) >= 2005).forEach(eachSeason -> allYears.add(eachSeason));
+                            this.regSeasons.stream()
+                                    .filter(eachSeason -> Integer.parseInt(eachSeason.substring(0, 4)) >= 1996)
+                                    .forEach(eachSeason -> allYears.add(eachSeason));
+                            this.postSeasons.stream()
+                                    .filter(eachSeason -> Integer.parseInt(eachSeason.substring(0, 4)) >= 1996)
+                                    .forEach(eachSeason -> allYears.add(eachSeason));
+                            this.preSeasons.stream()
+                                    .filter(eachSeason -> Integer.parseInt(eachSeason.substring(0, 4)) >= 2005)
+                                    .forEach(eachSeason -> allYears.add(eachSeason));
                             try {
+                                //Don't create table unless the player has been active once
                                 if (allYears.size() > 0 && Integer.parseInt(Collections.max(allYears).substring(0, 4)) >= 1996) {
                                     createIndividualDataTable(playerTableName);
                                     groupYears(allYears, playerTableName, reRun);
@@ -79,16 +100,22 @@ public class IndividualPlayerScraper implements ScraperUtilsInterface{
                                 LOGGER.error(ex.getMessage());
                             }
                         } catch (TimeoutException ex) {
-
+                            LOGGER.error(ex.getMessage());
                         }
                     }
                 } catch (Exception ex) {
-
+                    LOGGER.error(ex.getMessage());
                 }
             }
         }
     }
 
+    /**
+     * Fetches a player's associated URL
+     *
+     * @param id player ID
+     * @throws HttpTimeoutException If fetch times out
+     */
     protected void searchForPlayer(String id) throws HttpTimeoutException {
         try {
             this.regSeasons = new ArrayList<>();
@@ -112,7 +139,18 @@ public class IndividualPlayerScraper implements ScraperUtilsInterface{
         }
     }
 
-    private StringBuilder recordSeasons(JSONObject text, StringBuilder sb, String desiredString, ArrayList<String> list, String sbString, int eachIndex) {
+    /**
+     * Records a player's season activity data for a given season type
+     *
+     * @param text              the trimmed JSON response
+     * @param sb                the StringBuilder that builds the logging output
+     * @param desiredString     the keyword(s) that determine what years should be recorded
+     * @param list              list of active years for a season type
+     * @param loggingDescriptor what the logging output should be
+     * @param eachIndex         the index at which the desired data should be found
+     * @return the StringBuilder with the results of the method
+     */
+    private StringBuilder recordSeasons(JSONObject text, StringBuilder sb, String desiredString, ArrayList<String> list, String loggingDescriptor, int eachIndex) {
         String seasonType = text.getJSONArray("resultSets").getJSONObject(eachIndex).getString("name");
         String year;
         JSONArray rowSets;
@@ -128,30 +166,18 @@ public class IndividualPlayerScraper implements ScraperUtilsInterface{
                 }
             }
         }
-        if (!sb.toString().contains(sbString)) {
-            sb.append("(" + sbString + " : ").append(list.size()).append(") ");
+        if (!sb.toString().contains(loggingDescriptor)) {
+            sb.append("(" + loggingDescriptor + " : ").append(list.size()).append(") ");
         }
         return sb;
     }
 
-    public String generatePlayerSelectStatement(char startCharInclusive, char endCharInclusive, boolean onlyActivePlayers) throws Exception {
-        String alp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        int startCharIndex = alp.indexOf((startCharInclusive + "").toUpperCase());
-        int endCharIndex = alp.indexOf((endCharInclusive + "").toUpperCase());
-        if (endCharIndex < startCharIndex || startCharIndex < 0 || startCharIndex > 25 || endCharIndex < 0 || endCharIndex > 25) {
-            throw new IllegalArgumentException("Invalid selection.");
-        }
-        StringBuilder sbSelect = new StringBuilder(alp.charAt(startCharIndex) + "");
-        for (int i = startCharIndex + 1; i <= endCharIndex; i++) {
-            sbSelect.append("|").append(alp.charAt(i));
-        }
-        StringBuilder sqlSelect = new StringBuilder("SELECT * FROM player_relevant_data WHERE lastname REGEXP '^(" + sbSelect.toString() + ")'");
-        if (onlyActivePlayers) {
-            sqlSelect.append(" AND currentlyactive=1");
-        }
-        return sqlSelect.toString();
-    }
-
+    /**
+     * Inserts active seasons to each player's personal table
+     *
+     * @param playerTableName the player's table name
+     * @param year            the year to be inserted
+     */
     protected void insertGivenYear(String playerTableName, String year) {
         try {
             String sqlInsert = "INSERT INTO " + playerTableName + "(year) VALUES (?)";
@@ -163,10 +189,16 @@ public class IndividualPlayerScraper implements ScraperUtilsInterface{
             stmt.execute();
             LOGGER.info("Inserting " + playerTableName + " : " + year);
         } catch (SQLException ex) {
-
+            LOGGER.error(ex.getMessage());
         }
     }
 
+    /**
+     * Creates each player's personal data table
+     *
+     * @param playerTableName the table name
+     * @throws SQLException If the table creation fails
+     */
     protected void createIndividualDataTable(String playerTableName) throws SQLException {
         String createTable = "CREATE TABLE IF NOT EXISTS `" + playerTableName + "` (\n"
                 + "  `year` varchar(10) DEFAULT NULL,\n"
@@ -179,6 +211,13 @@ public class IndividualPlayerScraper implements ScraperUtilsInterface{
         connPlayers2.prepareStatement(createTable).execute();
     }
 
+    /**
+     * Updates the player's activity in their personal data table for a given season type
+     *
+     * @param playerTableName the table name
+     * @param year            the season
+     * @param seasonType      the season type
+     */
     protected void updateGivenSeasons(String playerTableName, String year, String seasonType) {
         try {
             String sqlUpdate = "UPDATE " + playerTableName + " SET " + seasonType + "= 1 WHERE year = \"" + year + "\"";
@@ -190,10 +229,16 @@ public class IndividualPlayerScraper implements ScraperUtilsInterface{
         }
     }
 
-    public void updateForCurrentYear( boolean onlyActivePlayers, String seasonType) {
+    /**
+     * Scrapes player data for only the current year
+     *
+     * @param onlyActivePlayers should only search for active players
+     * @param seasonType        the desired season type
+     */
+    public void updateForCurrentYear(boolean onlyActivePlayers, String seasonType) {
         while (true) {
             try {
-                HashMap<String, String> eachPlayerHashMap = RunHandler.popConcurrentHashMap();
+                HashMap<String, String> eachPlayerHashMap = RunHandler.popQueue();
                 if (eachPlayerHashMap == null) {
                     break;
                 }
@@ -212,10 +257,11 @@ public class IndividualPlayerScraper implements ScraperUtilsInterface{
                         existingTableCount++;
                     }
                 }
-
+                //Skip if table doesn't exist or player activity does not match boolean onlyActivePlayers
                 if (existingTableCount == 1 && (Integer.parseInt(eachPlayerHashMap.get("currentlyactive")) == 1) == onlyActivePlayers) {
                     ResultSet rsCurSeasActive = connPlayers1.prepareStatement("SELECT " + seasonType + " FROM " + playerTableName + " WHERE year = '" + ScraperUtilsInterface.super.getCurrentYear() + "'").executeQuery();
                     boolean shouldCont = true;
+                    //Will skip if player is already listed as being active in current year and season type
                     while (rsCurSeasActive.next()) {
                         if (rsCurSeasActive.getInt(seasonType) == 1) {
                             shouldCont = false;
@@ -236,23 +282,31 @@ public class IndividualPlayerScraper implements ScraperUtilsInterface{
                                     .forEach(eachSeason -> allYears.add(eachSeason));
                             groupYears(allYears, playerTableName, true);
                         } catch (TimeoutException ex) {
-
+                            LOGGER.error(ex.getMessage());
                         }
                     }
                 }
             } catch (Exception ex) {
-
+                LOGGER.error(ex.getMessage());
             }
         }
         LOGGER.info("End of updateForCurrentYear()");
     }
 
+    /**
+     * Organizes active years by season type and inserts them into the given table when not already present
+     * @param allYears all active years of a player's career
+     * @param playerTableName player's table name
+     * @param reRun should retrieve entries in the database that already exist
+     * @throws SQLException If adding seasons to the database fails
+     */
     private void groupYears(HashSet<String> allYears, String playerTableName, boolean reRun) throws SQLException {
         final String TABLE_NAME = playerTableName;
         HashSet<String> regularSeasonsInDB = new HashSet<>();
         HashSet<String> postSeasonsInDB = new HashSet<>();
         HashSet<String> preSeasonsInDB = new HashSet<>();
         HashSet<String> allSeasonsInDB = new HashSet<>();
+        //If rerunning, then check for existing entries in database to reduce database queries
         if (reRun) {
             ResultSet allYearData = connPlayers1.prepareStatement("SELECT * FROM " + playerTableName).executeQuery();
             while (allYearData.next()) {
@@ -268,6 +322,7 @@ public class IndividualPlayerScraper implements ScraperUtilsInterface{
                 }
             }
         }
+        //Insert year into table if it's above minimum threshold and is not already present
         allYears.stream().sorted()
                 .filter(eachYear -> Integer.parseInt(eachYear.substring(0, 4)) >= 1996 && !allSeasonsInDB.contains(eachYear))
                 .forEach(eachYear -> insertGivenYear(TABLE_NAME, eachYear));
@@ -282,6 +337,13 @@ public class IndividualPlayerScraper implements ScraperUtilsInterface{
                 .forEach(eachPre -> updateGivenSeasons(TABLE_NAME, eachPre, "preseason"));
     }
 
+    /**
+     * Retries searching for player data if original search times out
+     * @param eachID player ID
+     * @param firstName player first name
+     * @param lastName player last name
+     * @throws TimeoutException If fetch request times out
+     */
     private void loopSearchIfError(String eachID, String firstName, String lastName) throws TimeoutException {
         int exceptionRetryCounter = 0;
         while (exceptionRetryCounter < 3) {
